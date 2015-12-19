9c2bfe5ee4defd2c979f4e1fedef3d0eb98d6898
ArunSridharanDevOps <arun.sridharan@citi.com>
2015-12-19 13:22:23 -0600

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,10 @@
 file '/tmp/hello.txt3' do
   content 'Welcome to Chef Arun. Today is Friday'
 end
+
+bash 'mountfs' do
+  code <<-EOH
+  mount /dev/xvdi /apps
+  touch /tmp/fs.mounted
+    EOH
+  end
