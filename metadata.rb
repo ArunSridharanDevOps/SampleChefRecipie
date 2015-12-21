827ac1789dd3b865e441e5280821c3e6e33f1494
ArunSridharanDevOps <arun.sridharan@citi.com>
2015-12-20 20:42:51 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,5 @@
 name             'SampleChefRecipie'
 maintainer       'EDT'
-maintainer_email 'priyeshwani@gmail.com'
 license          'All rights reserved'
 description      'Hellow world'
 version          '0.1.0'
